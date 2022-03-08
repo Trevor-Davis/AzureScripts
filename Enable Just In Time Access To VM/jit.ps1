a55d2d8b60e78741f3c6601c6ed91bf5be074c57
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-03-08 12:55:15 -0500

    Update jit.ps1

--- Enable Just In Time Access To VM/jit.ps1
+++ Enable Just In Time Access To VM/jit.ps1
@@ -4,6 +4,27 @@ $sub = "3988f2d0-8066-42fa-84f2-5d72f80901da"
 $rg = "VirtualWorkloads-AVS"
 $region = "westus"
 
+function azurelogin {
+
+  param (
+      $subtoconnect
+  )
+$ErrorActionPreference = "SilentlyContinue"; $WarningPreference = "SilentlyContinue"
+
+$sublist = @()
+  $sublist = Get-AzSubscription
+  $checksub = $sublist -match $sub
+  If ($checksub.Count -eq 1 -and $checksub.id -eq $subtoconnect) {}
+  if ($checksub.Count -eq 1 -and $checksub.id -ne $subtoconnect) {Set-AzContext -Subscription $subtoconnect}
+  if ($checksub.Count -eq 0) {Connect-AzAccount -Subscription $subtoconnect}
+$ErrorActionPreference = "Continue"; $WarningPreference = "Continue"
+
+
+  }
+  
+azurelogin -subtoconnect $sub
+
+
 
 
 $JitPolicyVm1 = (@{
