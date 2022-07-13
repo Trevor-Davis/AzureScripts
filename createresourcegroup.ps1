b13b08f040c0fbe4522284d1b400447dfe51aa67
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-07-13 16:31:07 -0400

    adsf

--- createresourcegroup.ps1
+++ createresourcegroup.ps1
@@ -4,20 +4,21 @@ Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/AzureScri
 Clear-Host
 . $env:TEMP\AVSDeploy\$filename
 
-Add-Type -AssemblyName Microsoft.VisualBasic
-$sub = [Microsoft.VisualBasic.Interaction]::InputBox('Subscription:', ' ')
+if ($global:skipmanualinputs -ne "Yes") {
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    $sub = [Microsoft.VisualBasic.Interaction]::InputBox('Subscription:', ' ')
+    
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    $region = [Microsoft.VisualBasic.Interaction]::InputBox('Region to Create Resource Group:', ' ')
+    
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    $resourcegroupname = [Microsoft.VisualBasic.Interaction]::InputBox('Resource Group To Create:', ' ')
+}
 
-Add-Type -AssemblyName Microsoft.VisualBasic
-$rgregion = [Microsoft.VisualBasic.Interaction]::InputBox('Region to Create Resource Group:', ' ')
 
-Add-Type -AssemblyName Microsoft.VisualBasic
-$rgname = [Microsoft.VisualBasic.Interaction]::InputBox('Resource Group To Create:', ' ')
-
-New-AzResourceGroup -Name $rgname -Location $rgregion
+New-AzResourceGroup -Name $resourcegroupname -Location $region
 
 azurelogin -subtoconnect $sub
-
-
 
 #References
 #https://www.delftstack.com/howto/powershell/powershell-input-box/
