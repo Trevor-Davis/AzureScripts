aa29d5279886b38e371028a7c5f426bc83aa9a35
tredavis <trevor.davis@hotmail.com>
2022-12-20 11:10:08 -0500

    update

---
+++ Functions/Function-azurelogin.ps1
@@ -0,0 +1,14 @@
+#azure login function
+function azurelogin {
+
+  param (
+      $subtoconnect
+  )
+  $sublist = @()
+  $sublist = Get-AzSubscription -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+  $checksub = $sublist -match $subtoconnect
+  $getazcontext = Get-AzContext
+  If ($checksub.Count -eq 1 -and $getazcontext.Subscription.Id -eq $subtoconnect) {" "}
+  if ($checksub.Count -eq 1 -and $getazcontext.Subscription.Id -ne $subtoconnect) {Set-AzContext -SubscriptionId $subtoconnect -WarningAction SilentlyContinue}
+  if ($checksub.Count -eq 0) {Connect-AzAccount -Subscription $subtoconnect -WarningAction SilentlyContinue}
+  }
